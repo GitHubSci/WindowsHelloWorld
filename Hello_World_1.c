176ee102f1782e745c1526aa9502d8005464ee95
Peter HomeOffice <HomeOffice_Sci@t-online.de>
2023-04-05 14:15:26 +0200

    Firt CheckIn

---
+++ Hello_World_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf(" Kommt der jetzt immer noch durch oder nicht ? \n");
+	printf(" klappt der Rückweg ? \n");
+	printf(" Ja - hat geklappt !! \n");
+	printf(" ... Super und gut... !! \n");	
+	printf(" ... naja... !! \n");	
+	printf(" ... dann ein neuer Versuch... !! \n");	
+	printf(" ... was passiert denn jetzt .. ? \n");
+	printf(" ... hat das jetzt auch geklappt .. ? \n");
+	printf(" ... für neuen Plugin-Test .. ? \n");
+return 0;
+}
+ 
